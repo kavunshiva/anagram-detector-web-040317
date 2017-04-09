199b7e8a1f477755a8779a663fcbf8525751f7e0
kavunshiva <kavunshiva@gmail.com>
2017-04-09 16:00:00 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+  def initialize(word)
+    @alphabetized_word = alphabetize(word)
+  end
+
+  def alphabetize(word)
+    word.split("").sort.join
+  end
+
+  def match(word_list)
+    word_list.select do |word|
+      @alphabetized_word == alphabetize(word)
+    end
+  end
+end
